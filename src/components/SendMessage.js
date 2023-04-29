380134778608eb5a3fcab7204dd80b94ca485131
rakudaBombayer <kodama23m@icloud.com>
2023-04-29 22:24:00 +0900

    動画と違うがなんとかデータベースにテキストを送れた段階

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {db, auth } from "../firebase.js";
+import firebase from "firebase/compat/app";
 
 function SendMessage() {
+  const [message, setMessage] = useState("");
+  function SendMessage(e) {
+    e.preventDefault();
+
+    const {uid, photoURL} = auth.currentUser;
+
+    db.collection("messages").add({
+      text: message,
+      photoURL,
+      uid,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+  }
   return (
     <div>
-      <form>
-        <div className="sendMsg"></div>
-        <input placeholder='メッセージを入力してください。'/>
+      <form onSubmit={SendMessage}>
+      <input className="sendMsg" type="text" placeholder='メッセージを入力してください' value={message} onChange={(e) => setMessage(e.target.value)}/>
       </form>
     </div>
   )
